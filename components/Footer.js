cc5d1f17457c917c2a9f4e0af7a7b006a0422321
Justin Mather <justin.mather@gmail.com>
2022-02-18 10:05:32 -0800

    Stub out header, footer components

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer(props) {
+  return (
+    <footer>
+      <div class="credits">
+        <span>&copy; 2022 <a href="/">Justin Mather</a></span>
+      </div>
+    </footer>
+  )
+}
---
+++ components/Header.js
@@ -0,0 +1,9 @@
+export default function Header(props) {
+  return (
+    // Header and nav template site-wide
+    <header>
+      <h1 class="header-title"><a href="/">itsjustmath.net</a></h1>
+      <h2 class="header-subtitle">Observations and advice on navigating the ever-changing digital landscape.</h2>
+    </header>
+  )
+}
