6f956c47a7f3705d4deaebab26a11fe4c8beab12
Justin Mather <justin.mather@gmail.com>
2022-03-04 08:37:33 -0800

    Add newsletter signup form to footer
    Closes #22

--- components/Footer.js
+++ components/Footer.js
@@ -5,6 +5,29 @@ export default function Footer(props) {
         <span>
           &copy; 2022 <a href="/">Justin Mather</a>
         </span>
+
+        <form
+          action="https://buttondown.email/api/emails/embed-subscribe/itsjustmath"
+          method="post"
+          target="popupwindow"
+          onsubmit="window.open('https://buttondown.email/itsjustmath', 'popupwindow')"
+          class="embeddable-buttondown-form"
+          style={{
+            display: "inline-block",
+            marginLeft: "10px",
+          }}
+        >
+          <label for="bd-email">
+            <strong>Newsletter Sign-up: </strong>
+          </label>
+          <input type="email" name="email" id="bd-email" placeholder="Email" />
+          <input type="hidden" value="1" name="embed" />
+          <input
+            type="submit"
+            value="Subscribe"
+            style={{ marginLeft: "3px" }}
+          />
+        </form>
       </div>
     </footer>
   );
