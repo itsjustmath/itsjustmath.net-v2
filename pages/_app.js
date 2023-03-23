9b79d5894eb43f43ccce666600e53ad395eee03b
Justin Mather <itsjustmath@users.noreply.github.com>
2023-03-23 07:41:41 -0700

    Feature | SEO + Analytics (#76)
    * Update deps, switch to yarn
    
    * Add GA
    
    * Baseline SEO implementation
    
    * Fix homepage mobile image issue
    
    Closes #26
    
    * Add some fallback SEO values for posts

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
-import '../styles/global.scss'
+import { GoogleAnalytics } from "nextjs-google-analytics";
+import { DefaultSeo } from "next-seo";
+import { config } from "../config";
+import "../styles/global.scss";
 
 export default function App({ Component, pageProps }) {
   // Use the layout defined at the page level, if available
@@ -6,5 +9,25 @@ export default function App({ Component, pageProps }) {
 
   // REVIEW: what does the fallback layout look like?
 
-  return getLayout(<Component {...pageProps} />);
+  return getLayout(
+    <>
+      <DefaultSeo
+        title={config.title}
+        description={config.description}
+        openGraph={{
+          url: "https://itsjustmath.net/",
+          title: config.title,
+          description: config.description,
+          site_name: "itsjustmath.net", // REVIEW: or call it Justin Mather?
+        }}
+        twitter={{
+          handle: "@itsjustmath",
+          site: "@itsjustmath",
+          cardType: "summary_large_image",
+        }}
+      />
+      <GoogleAnalytics trackPageViews />
+      <Component {...pageProps} />
+    </>
+  );
 }
